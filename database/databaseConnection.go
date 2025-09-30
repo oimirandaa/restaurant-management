a194f5365ebb1dcc641b17780724cb388c0fa536
Oscar Miranda <oimirandaa08@gmail.com>
2025-09-30 15:12:35 -0600

    Create database connection

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,8 +1,42 @@
 package database
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
-func OpenCollection(client any, s string) *mongo.Collection {
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
 
-	return
+func DBInstance() *mongo.Client {
+	MongoDb := "mongodb://localhost:27017"
+	fmt.Print(MongoDb)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		return nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+
+	return collection
 }
