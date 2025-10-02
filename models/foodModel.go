ed34650e908d104b940ac5a513ccd5c67db8b7d4
Oscar Miranda <oimirandaa08@gmail.com>
2025-10-02 17:38:11 -0600

    Created all the structs for the models

--- models/foodModel.go
+++ models/foodModel.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Food struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	Name      *string            `json:"name" validate:"required,min=2,max=100"`
+	Price     *float64           `json:"price" validate:"required,min=0,max=100000"`
+	FoodImage *string            `json:"food_image" validate:"required,min=2,max=1000"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at"`
+	FoodId    string             `json:"food_id"`
+	MenuId    *string            `json:"menu_id" validate:"required"`
+}
--- models/menuModel.go
+++ models/menuModel.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Menu struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	Name      string             `json:"name" validate:"required,min=2,max=100"`
+	Category  string             `json:"category" validate:"required"`
+	StartDate *time.Time         `json:"start_date" validate:"required"`
+	EndDate   *time.Time         `json:"end_date" validate:"required"`
+	CreatedAt time.Time          `json:"created_at" validate:"required"`
+	UpdatedAt time.Time          `json:"updated_at" validate:"required"`
+	MenuId    string             `json:"food_id"`
+}
