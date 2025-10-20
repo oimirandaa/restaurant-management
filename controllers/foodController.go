a5ac0d8fc5536edac9b294e45fe60c8afb21bca8
Oscar Miranda <oimirandaa08@gmail.com>
2025-10-19 18:30:31 -0600

    Finish foodController.go by adding the GetFoods, UpdateFoods and fixed functions

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -3,9 +3,12 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"log"
+	"math"
 	"net/http"
 	"restaurant-management-system/database"
 	"restaurant-management-system/models"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -13,14 +16,62 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "foods")
+var foodCollection = database.OpenCollection(database.Client, "foods")
 var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10
+		}
+
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{"$match", bson.D{}}}
+		groupStage := bson.D{
+			{"$group", bson.D{
+				{"$_id", bson.D{
+					{"_id", "null"}}},
+				{"total_count", bson.D{
+					{"$sum", 1}}},
+				{"data", bson.D{
+					{"$push", "$$ROOT"}}},
+			}}}
+		projectStage := bson.D{
+			{"$project", bson.D{
+				{"_id", 0},
+				{"total_count", 1},
+				{"food_items", bson.D{{"$slice", []interface{}{"data", startIndex, recordPerPage}}}},
+			}}}
+
+		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage,
+			groupStage,
+			projectStage,
+		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing the food items"})
+		}
+
+		var allFoods []bson.M
+		if err = result.All(ctx, &allFoods); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allFoods[0])
 	}
 }
 
@@ -79,22 +130,83 @@ func CreateFood() gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 		}
-		c.JSON(http.StatusCreated, result)
 
+		c.JSON(http.StatusCreated, result)
 		return
 	}
 }
 
 func UpdateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var food models.Food
+		var menu models.Menu
+
+		foodId := c.Param("food_id")
+		defer cancel()
+
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var updatedObj primitive.D
+
+		if food.Name != nil {
+			updatedObj = append(updatedObj, bson.E{Key: "name", Value: food.Name})
+		}
+		if food.Price != nil {
+			updatedObj = append(updatedObj, bson.E{Key: "price", Value: food.Price})
+		}
+		if food.FoodImage != nil {
+			updatedObj = append(updatedObj, bson.E{Key: "food_image", Value: food.FoodImage})
+		}
+		if food.MenuId != nil {
+			err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.MenuId}).Decode(&menu)
+			defer cancel()
+
+			if err != nil {
+				msg := fmt.Sprintf("menu with id %s does not exist", food.MenuId)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				return
+			}
+
+			updatedObj = append(updatedObj, bson.E{Key: "menu_id", Value: food.MenuId})
+		}
+
+		food.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updatedObj = append(updatedObj, bson.E{Key: "updated_at", Value: food.UpdatedAt})
+
+		upsert := true
+		filter := bson.M{"food_id": foodId}
+
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		result, err := foodCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{"$set", updatedObj},
+			},
+			&opt,
+		)
+		if err != nil {
+			msg := fmt.Sprintf("error ocurred while updating the food item %s", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		c.JSON(http.StatusOK, result)
 
 	}
 }
 
 func round(num float64) int {
-	return 0
+	return int(num + math.Copysign(0.5, num))
 }
 
 func toFixed(num float64, precision int) float64 {
-	return 0
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
 }
