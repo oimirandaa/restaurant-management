f61e9f8ffe7bdda4b968d2adc66d4dfe07977118
Oscar Miranda <oimirandaa08@gmail.com>
2025-11-08 18:05:38 -0600

    Added auth and token helpers

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,1 +1,116 @@
 package helpers
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"context"
+	"log"
+	"os"
+	"restaurant-management-system/database"
+	"time"
+)
+
+type SignedDetails struct {
+	Email     string
+	FirstName string
+	LastName  string
+	Uid       string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, fistName string, lastName string, uid string) (signedToken, refreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:     email,
+		FirstName: fistName,
+		LastName:  lastName,
+		Uid:       uid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	signedToken, err = jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return "", "", err
+	}
+	refreshToken, err = jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return "", "", err
+	}
+
+	return signedToken, refreshToken, err
+}
+
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var updateObj = primitive.D{}
+
+	updateObj = append(updateObj, primitive.E{Key: "signed_token", Value: signedToken})
+	updateObj = append(updateObj, primitive.E{Key: "signed_refresh_token", Value: signedRefreshToken})
+
+	updatedAt, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, primitive.E{Key: "updated_at", Value: updatedAt})
+	filter := bson.M{"user_id": userId}
+	upsert := true
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	_, err := userCollection.UpdateOne(
+		ctx,
+		filter,
+		bson.D{
+			primitive.E{Key: "$set", Value: updateObj},
+		},
+		&opt,
+	)
+	if err != nil {
+		log.Panic(err)
+		return
+
+	}
+}
+
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(SECRET_KEY), nil
+		},
+	)
+
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = fmt.Sprintf("Invalid token claims")
+		msg = err.Error()
+		return nil, msg
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = fmt.Sprintf("Token expired")
+		msg = err.Error()
+		return nil, msg
+	}
+
+	return claims, msg
+}
--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,7 +1,34 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"restaurant-management-system/helpers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No token found in header")})
+			c.Abort()
+			return
+		}
 
+		claims, err := helpers.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.FirstName)
+		c.Set("last_name", claims.LastName)
+		c.Set("uid", claims.Uid)
+
+		c.Next()
+	}
 }
